e3697eefaf9522b18036dc994f1e3c85b87be1f9
joowojr <joowojr@gmail.com>
2024-12-14 07:50:29 +0000

    🚑 hotfix: redis db 설정 변경

--- infrastructure/redis/redis_connection.py
+++ infrastructure/redis/redis_connection.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 REDIS_HOST = os.getenv('REDIS_HOST', 'localhost') 
 REDIS_PORT = int(os.getenv('REDIS_PORT', 6379)) 
-REDIS_DB = int(os.getenv('REDIS_DB', 4))
+REDIS_DB = int(os.getenv('REDIS_DB', 5))
 REDIS_PASSWORD = os.getenv('REDIS_PASSWORD')
 
 def get_redis_connection():
